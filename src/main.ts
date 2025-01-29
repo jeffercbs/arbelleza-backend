fb830b03968f1b03a3bb20cd1d5dd4378f38a934
Jeferson Barrero <jebarrero14@gmail.com>
2025-01-29 15:31:08 -0500

    Change default port from 3000 to 4000 in main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ const clerkClient = createClerkClient({
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
+
   app.enableCors({
     origin: ['http://localhost:3001', 'https://arbelleza.com'],
   });
