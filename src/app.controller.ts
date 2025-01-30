b4e81d645d094280986e365e71487fa983fd4127
Jeferson Barrero <jebarrero14@gmail.com>
2025-01-30 09:00:00 -0500

    Add authentication decorators for enhanced access control on getHello endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Keys, View } from './auth/decorator';
+import { Key } from './auth/enum/key.enum';
+import { Visibility } from './auth/visibility.enum';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Keys(Key.NotApiKey)
+  @View(Visibility.Public)
   getHello(): string {
     return this.appService.getHello();
   }
